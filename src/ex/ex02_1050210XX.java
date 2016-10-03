1354a4d61d5d0df4bee884e08e07bce57416672d
sthe283156w@icloud.com <opkm62489>
2016-10-03 15:41:18 +0800

    105025048 溫宜庭

--- src/ex/ex02_1050210XX.java
+++ src/ex/ex02_1050210XX.java
@@ -1,15 +1,39 @@
 package ex;
+
+import java.util.Scanner;
+
 /*
  * Topic: 輸入一個字元，判斷是大寫或小寫或是其他字元。
  * Date: 2016/10/03
- * Author: 1050210XX 周永振老師
+ * Author: 105025048 溫宜庭
  */
 
 public class ex02_1050210XX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		char ch1 = scn.next().charAt(0);
+		int v1=ch1-'a';
+		if(v1>=0 &&v1<26){
+			System.out.print("小寫");
+			
+			
+		
+		}else{
+			
+			v1 = ch1-'A';
+			if(v1>=0&&v1<26){
+				System.out.print("大寫");
+					
+					
+		}else{
+			
+			
+			
+				}
+			}
+		}
 
 	}
 
-}
